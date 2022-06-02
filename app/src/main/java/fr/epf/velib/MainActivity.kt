e5ed6e6eb5b78f7ddd98171a91dcd326b742b432
leabruchon <leabruchon@icloud.com>
2022-06-02 16:40:20 +0200

    map import home activity

--- app/src/main/java/fr/epf/velib/MainActivity.kt
+++ app/src/main/java/fr/epf/velib/MainActivity.kt
@@ -3,6 +3,13 @@ package fr.epf.velib
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import fr.epf.velib.api.ApiService
 import fr.epf.velib.model.StationVelib
 import kotlinx.coroutines.runBlocking
@@ -11,11 +18,16 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+    private val PERTH = LatLng(-31.952854, 115.857342)
+    private var markerPerth: Marker? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
         callApi()
     }
 
@@ -41,4 +53,43 @@ class MainActivity : AppCompatActivity() {
         }
         return stations
     }
+
+    /** Called when the map is ready.  */
+    override fun onMapReady(map: GoogleMap) {
+
+        // Add some markers to the map, and add a data object to each marker.
+        markerPerth = map.addMarker(
+            MarkerOptions()
+                .position(PERTH)
+                .title("Perth")
+        )
+        markerPerth?.tag = 0
+
+
+        // Set a listener for marker click.
+        map.setOnMarkerClickListener(this)
+    }
+
+    /** Called when the user clicks a marker.  */
+    override fun onMarkerClick(marker: Marker): Boolean {
+
+        // Retrieve the data from the marker.
+        val clickCount = marker.tag as? Int
+
+        // Check if a click count was set, then display the click count.
+        clickCount?.let {
+            val newClickCount = it + 1
+            marker.tag = newClickCount
+            Toast.makeText(
+                this,
+                "${marker.title} has been clicked $newClickCount times.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        return false
+    }
 }
+
+
+
+
